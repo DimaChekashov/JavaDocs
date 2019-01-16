2c996eac328cc7a1d50f053d0744ffea5d7f979b
DimaChekashov <foxdima99@gmail.com>
2019-01-17 01:25:46 +0300

    update menu and add fragment navigation

--- app/src/main/java/mymessenger/hfad/com/javadocs/MainActivity.java
+++ app/src/main/java/mymessenger/hfad/com/javadocs/MainActivity.java
@@ -1,13 +1,16 @@
 package mymessenger.hfad.com.javadocs;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -20,11 +23,42 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Chapter1Fragment()).commit();
+            navigationView.setCheckedItem(R.id.chapter_1);
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chapter_1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Chapter1Fragment()).commit();
+                break;
+            case R.id.chapter_2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Chapter2Fragment()).commit();
+                break;
+            case R.id.chapter_3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Chapter3Fragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
